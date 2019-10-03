4f6decb9dc622c32cdd6aa1bbbb7ecd9e9de9f55
idontknowusername <44739864+idontknowusername@users.noreply.github.com>
2019-10-03 13:42:05 +0900

    뷰 적용 1차 pull (#5)
    * 모듈 수정
    
    package.json
    package-lock.json
    
    * 경로 변경에 의한 파일삭제
    
    * asset 파일 추가
    
    경로 변경에 의해 삭제한 asset 파일 다시 추가
    
    * 메인 파일
    
    app.js
    config.js
    
    * 데이터베이스 추가
    
    database/database.js
    database/user_schema.js
    
    ! 사용 컬렉션 명 : users5
    
    * 라우트 함수 추가
    
    routes/route_loeader.js
    routes/test.js
    routes/user.js
    
    * View 파일 추가
    
    사용하는 파일 리스트
    views/indexs.ejs
    views/profile.ejs
    views/signup.ejs
    
    이후에 파일 정리 필요함

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	server_port: 3000,
+	db_url: 'mongodb://localhost:27017/local',
+	db_schemas: [
+	    {file:'./user_schema', collection:'users5', schemaName:'UserSchema', modelName:'UserModel'}
+	],
+	route_info: [
+	]
+}
